6fe0c1362b24004384cfa2402592cc99e59c3236
Adesh Pandey <Adesh Pandey>
2016-04-07 22:40:10 +0530

    very basic fxml integrated

--- src/dbms/Main.java
+++ src/dbms/Main.java
@@ -1,10 +1,30 @@
 package dbms;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	@Override
+	public void start(Stage primaryStage) {
+		
+		try {
+		
+			Parent root = FXMLLoader.load(getClass().getResource("Filter.fxml"));
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Filter");
+            primaryStage.show();
+        } catch (Exception ex) {
+        	ex.printStackTrace();
 
+        }
+		
 	}
 
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
