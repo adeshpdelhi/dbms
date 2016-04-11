5f4b1c3ee422585e405b2f16c67cd5a99945cd2e
Rishabh <rishabh999gupta@gmail.com>
2016-04-11 15:04:54 +0530

    Adding fxml updated controller

--- src/dbms/Controller.java
+++ src/dbms/Controller.java
@@ -1,5 +1,70 @@
+
 package dbms;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+
+public class Controller implements Initializable{
+
+    @FXML private ToggleGroup plans_type;
+    @FXML
+    private TextField users_age_from;
+    @FXML
+    private RadioButton insertuser_gender_male;
+    @FXML private RadioButton insertuser_type_prepaid;
+    @FXML private RadioButton plans_type_prepaid;
+    @FXML private CheckBox plans_calls_3;
+    @FXML    private Button changeplan_submit;
+    @FXML     private CheckBox plans_calls_4;
+    @FXML    private ToggleGroup insertuser_type;
+    @FXML    private CheckBox plans_calls_1;
+    @FXML    private CheckBox plans_calls_2;
+    @FXML    private TextField users_city;
+    @FXML    private TextField queries_1_k;
+    @FXML    private CheckBox queries_3_gt;
+    @FXML    private CheckBox plans_msg_1;
+    @FXML    private TextArea insertuser_address;
+    @FXML    private TextField changeplan_planid;
+    @FXML    private ToggleGroup insertuser_type1;
+    @FXML    private TextField plans_planid;
+    @FXML    private TextField queries_3_lt_value;
+    @FXML     private TextField queries_3_eq_value;
+    @FXML    private RadioButton insertuser_type_postpaid;
+    @FXML    private RadioButton insertuser_plan_planid;
+    @FXML    private RadioButton plans_type_postpaid;
+    @FXML    private TextField queries_2_k;
+    @FXML    private CheckBox queries_3_eq;
+    @FXML    private TextField queries_4_k;
+    @FXML    private TextField insertuser_city;
+    @FXML    private TextField queries_4_m;
+    @FXML    private TextField insertuser_plan_planid_value;
+    @FXML    private Button queries_submit;
+    @FXML    private DatePicker users_usersince;
+    @FXML    private ToggleGroup insertuser_gender;
+    @FXML    private TextField insertuser_age;
+    @FXML    private CheckBox queries_4;
+    @FXML    private CheckBox plans_data_1;
+    @FXML    private CheckBox queries_3;
+    @FXML    private CheckBox queries_3_lt;
+    @FXML    private CheckBox queries_2;
+    @FXML    private CheckBox queries_1;
+    @FXML    private TextField changeplan_mobile;
+    @FXML    private CheckBox queries_5;
+    @FXML    private Tab insertuser_name;
+    @FXML    private TextField plans_price_from;
+    @FXML   private RadioButton insertuser_plan_basicplan;
+    @FXML    private RadioButton insertuser_gender_female;
+    @FXML    private Tab users_mobile;
+    @FXML    private TextField plans_price_to;
+    @FXML    private TextField insertuser_mobile;
+    @FXML    private TextField queries_3_gt_value;
 
 }
+
